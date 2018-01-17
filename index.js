e3d8aace24c4a0e03b3fdd6fb66230905aafd326
Mehwish Kapadia <mehwishkap@gmail.com>
2018-01-17 19:32:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function whisper (string){
+return string.toLowerCase()
+}
+function shout (string){
+return string.toUpperCase()
+}
+function sayHiToGrandma (string) {
+  var lowercase = "hello!"
+  lowercase.toLowerCase()===lowercase 
+  return ("I can't hear you!")
+}
+function sayHiToGrandma (STRING){
+  var uppercase = "Hello!"
+  uppercase.toUpperCase()=== uppercase
+  return ("Yes Indeed!")
+}
+function sayHiToGrandma (STRING){
+  var mixedCase = "I love you, Grandma"
+}
