c1aff8795bd55254cc54f23802c7fbf819392345
Mehwish Kapadia <mehwishkap@gmail.com>
2018-01-17 20:10:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ return string.toUpperCase()
 }
 function sayHiToGrandma (string) {
   if (string===string.toLowerCase())
-  return ("I can\'t hear you!")}
-   else if (string=== string.toUpperCase()){
+  return ("I can\'t hear you!")
+  
+}   else if (string=== string.toUpperCase()){
   return ("YES INDEED!")}
   else {
     return ("I love you, too.")
