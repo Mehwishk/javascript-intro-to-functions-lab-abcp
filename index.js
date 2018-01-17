7096940c73e4d7aa1883c9568fd8f3cad50c2113
Mehwish Kapadia <mehwishkap@gmail.com>
2018-01-17 19:55:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ return string.toUpperCase()
 function sayHiToGrandma (string) {
   var lowercase = "hello!"
   lowercase.toLowerCase()===lowercase 
-  return ("I can't hear you!")
+  return ("I can\'t hear you!")
 }
 function sayHiToGrandma (STRING){
   var uppercase = "Hello!"
